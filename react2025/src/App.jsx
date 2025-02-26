df48c88d9dc8c49e89078f2336995b52ed7ecc5f
abdul <abduldesigns24@gmail.com>
2025-02-26 19:16:06 +0530

    Add Tailwind CSS support and update App component with state management

--- react2025/src/App.jsx
+++ react2025/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // function App() {
 //   let i = 0;
 //   function counter() {
@@ -18,7 +20,22 @@
 // export default App;
 
 const App = () => {
-  return <div>App</div>;
+  let i = 1;
+  let [count, setCount] = useState(i);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <button
+        onClick={() => {
+          console.log(i);
+          setCount(count + 10);
+        }}
+      >
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default App;
