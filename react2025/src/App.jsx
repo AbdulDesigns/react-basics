b882e72826c0d6af060b2af4813ff3b59815cca0
abdul <abduldesigns24@gmail.com>
2025-02-27 00:05:16 +0530

    Add form handling and input for fullname in App component

--- react2025/src/App.jsx
+++ react2025/src/App.jsx
@@ -21,12 +21,31 @@ import { useState } from "react";
 
 const App = () => {
   let i = 1;
-  let [count, setCount] = useState(i);
+  let [count, setCount] = useState("");
+  function formHandler(e) {
+    e.preventDefault();
+    console.log(count);
+    setCount("");
+  }
   return (
-    <div>
+    <>
       <h1>{count}</h1>
       <h1 className="text-5xl font-bold underline">Hello world!</h1>
+      <form action="/" onSubmit={formHandler}>
+        <input
+          type="text"
+          value={count}
+          placeholder="enter fullname"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+
       <button
+        className="bg-blue-500 text-white p-4 rounded-lg mx-4 my-4 cursor-pointer"
         onClick={() => {
           console.log(i);
           setCount(count + 10);
@@ -34,7 +53,7 @@ const App = () => {
       >
         click me
       </button>
-    </div>
+    </>
   );
 };
 
